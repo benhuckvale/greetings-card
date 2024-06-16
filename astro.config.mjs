1b3cda263e401cb19a5a88d5ff9dcb28a71dfb36
Ben Huckvale <5511911+benhuckvale@users.noreply.github.com>
2024-06-16 12:17:09 +0100

    Try using base setting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://benhuckvale.github.io/greetings-card',
+  site: 'https://benhuckvale.github.io/',
+  base: '/greetings-card/',
   output: 'static',
   devToolbar: {
     enabled: false
