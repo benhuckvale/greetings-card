b2c34cf28138a3c2ed0d779e3675ceb4949904b5
Ben Huckvale <5511911+benhuckvale@users.noreply.github.com>
2024-06-16 08:35:59 +0100

    Use react-pageflip library

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GreetingsCard from './components/GreetingsCard.jsx';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <GreetingsCard />
+    </div>
+  );
+};
+
+export default App;
+
